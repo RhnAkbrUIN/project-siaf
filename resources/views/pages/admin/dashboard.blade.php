886fc87f96d18b6988e9ec0d5caeec52c22c68a5
anggia <anggiavira@gmail.com>
2023-12-08 20:48:28 +0700

    tes ubah dashboard admin

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -5,7 +5,7 @@
 <main>
 	<div class="head-title">
 		<div class="left">
-			<h1>Dashboard Admin</h1>
+			<h1>Dashboard Admin Kampus</h1>
 			<ul class="breadcrumb">
 				<li>
 					<a href="#">Dashboard</a>
